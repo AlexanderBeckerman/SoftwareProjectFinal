db0e34d42a392d308df0401169da816ed46625ce
beckermana <beckermana@mail.tau.ac.il>
2023-02-21 19:13:15 +0200

    iii

---
+++ main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include "project_header.h"
+
+
+int main(int argc, char *argv[]) {
+
+    char * command = argv[1];
+    char * filename = argv[2];
+    double n;
+    int numpoints;
+    char c;
+    struct vector *head_vec;
+    struct vector *curr_vec;
+    struct cord *head_cord;
+    struct cord *curr_cord;
+
+    FILE* file = fopen(filename, "r");
+    if(file == NULL){
+        printf("open file error");
+        return 0;
+    }
+
+    head_cord = malloc(sizeof( Cord));
+    curr_cord = head_cord;
+    curr_cord->next = NULL;
+
+    head_vec = malloc(sizeof( Vector));
+    curr_vec = head_vec;
+    curr_vec->next = NULL;
+
+    while(fscanf(file, "%lf%c", &n, &c) == 2){
+        if (c == '\n')
+        {
+            numpoints++;
+            curr_cord->value = n;
+            curr_vec->cords = head_cord;
+            curr_vec->next = malloc(sizeof( Vector));
+            curr_vec = curr_vec->next;
+            curr_vec->next = NULL;
+            curr_vec->cords = NULL;
+            head_cord = malloc(sizeof( Cord));
+            curr_cord = head_cord;
+            curr_cord->next = NULL;
+            continue;
+        }
+
+        curr_cord->value = n;
+        curr_cord->next = malloc(sizeof( Cord));
+        curr_cord = curr_cord->next;
+        curr_cord->next = NULL;
+    }
+
+
+
+
+
+
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ wam.c
@@ -0,0 +1,50 @@
+#include "project_header.h"
+
+double calcDistance(Vector*,Vector*);
+
+int wam(Vector * head, int numpoints){
+    Vector * curr1 = head;
+    int row=0;
+    int col=0;
+    int i,j;
+    Vector * curr2 = curr1;
+    double matrix[numpoints][numpoints];
+    while(curr1 != NULL){
+        while(curr2 != NULL){
+            if(curr1 == curr2) {
+                matrix[row][row] = 0;
+                col++;
+                continue;
+            }
+            double distance = calcDistance(curr1, curr2);
+            double exponent = -1*(distance/2);
+            matrix[row][col] = exp(exponent);
+            col++;
+            curr2 = curr2->next;
+        }
+        curr2 = head;
+        col = 0;
+        row++;
+        curr1 = curr1->next;
+    }
+    for(i=0;i<numpoints;i++){
+        for(j=0;j<numpoints;j++){
+            printf("row: %i col: %i value: %f ", i,j,matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+}
+
+double calcDistance(Vector * point1,  Vector * point2){
+    double diff = 0;
+    struct cord * pointCords = point1->cords;
+    struct cord * centroidCords = point2->cords;
+    while(pointCords != NULL){
+        diff += pow(pointCords->value - centroidCords->value, 2);
+        pointCords = pointCords->next;
+        centroidCords = centroidCords->next;
+    }
+    diff = sqrt(diff);
+    return diff;
+}
