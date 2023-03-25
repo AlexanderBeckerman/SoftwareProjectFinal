85f23393bcd3474f18b85f128f6f71bff2c42043
Yonatan Goldenberg <yonatan.r.gold@gmail.com>
2023-03-25 14:59:38 +0300

    more fixes

--- ddg.c
+++ ddg.c
@@ -2,13 +2,14 @@
 
 double ** ddg(Vector * points, int n)
 {
+    double sum;
     double ** weighted = wam(points, n);
     double ** diagonal = calloc(n , sizeof (double *));
     int i, j;
     for (i = 0; i < n; i++)
     {
         diagonal[i] = calloc(n , sizeof (double));
-        double sum = 0;
+        sum = 0;
         for (j = 0; j < n; j++)
             sum += weighted[i][j];
         diagonal[i][i] = sum;
