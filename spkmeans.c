5f4e254db67ae5da7fcfaf78500bae38ecc956c3
beckermana <beckermana@mail.tau.ac.il>
2023-03-25 15:48:34 +0300

    Update spkmeans.c

--- spkmeans.c
+++ spkmeans.c
@@ -6,13 +6,20 @@ int main(int argc, char *argv[]) {
     char * command = argv[1];
     char * filename = argv[2];
     double n;
+    int i;
+    int j;
     int numpoints=0;
     char c;
+    double ** jacboi_mat;
     struct vector *head_vec;
     struct vector *curr_vec;
     struct cord *head_cord;
     struct cord *curr_cord;
 
+    if(argc < 2){
+        printf("invalid arguments");
+        return -1;
+    }
     FILE* file = fopen(filename, "r");
     if(file == NULL){
         printf("open file error");
@@ -26,28 +33,39 @@ int main(int argc, char *argv[]) {
     head_vec = malloc(sizeof( Vector));
     curr_vec = head_vec;
     curr_vec->next = NULL;
-
     while(fscanf(file, "%lf%c", &n, &c) == 2){
-        if (c == '\n')
-        {
-            numpoints++;
+            if (c == '\n')
+            {
+                numpoints++;
+                curr_cord->value = n;
+                curr_vec->cords = head_cord;
+                curr_vec->next = malloc(sizeof( Vector));
+                curr_vec = curr_vec->next;
+                curr_vec->next = NULL;
+                curr_vec->cords = NULL;
+                head_cord = malloc(sizeof( Cord));
+                curr_cord = head_cord;
+                curr_cord->next = NULL;
+                continue;
+            }
+
             curr_cord->value = n;
-            curr_vec->cords = head_cord;
-            curr_vec->next = malloc(sizeof( Vector));
-            curr_vec = curr_vec->next;
-            curr_vec->next = NULL;
-            curr_vec->cords = NULL;
-            head_cord = malloc(sizeof( Cord));
-            curr_cord = head_cord;
+            curr_cord->next = malloc(sizeof( Cord));
+            curr_cord = curr_cord->next;
             curr_cord->next = NULL;
-            continue;
         }
 
-        curr_cord->value = n;
-        curr_cord->next = malloc(sizeof( Cord));
-        curr_cord = curr_cord->next;
-        curr_cord->next = NULL;
+    curr_vec = head_vec;
+    jacboi_mat = calloc(numpoints, sizeof(double *));
+    for(i = 0; i < numpoints; i ++){
+        jacboi_mat[i] = calloc(numpoints, sizeof(double));
+        for(j=0; j < numpoints; j++){
+            jacboi_mat[i][j] = curr_vec->cords->value;
+            curr_vec->cords = curr_vec->cords->next;
+        }
+        curr_vec = curr_vec->next;
     }
+
 
 
     fclose(file);
@@ -60,7 +78,7 @@ int main(int argc, char *argv[]) {
     else if (strcmp(command, "gl") == 0)
         gl(head_vec, numpoints);
     else if (strcmp(command, "jacobi") == 0)
-        jacobi(gl(head_vec, numpoints), numpoints);
+        jacobi(jacboi_mat, numpoints);
     else {
         printf("--- Unkonown Command ---");
         return -1;
