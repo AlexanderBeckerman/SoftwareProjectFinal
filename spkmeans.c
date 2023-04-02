629f1932300cc6ba44248b826960b3d9d1adc940
beckermana <beckermana@mail.tau.ac.il>
2023-04-02 15:09:37 +0300

    Update spkmeans.c

--- spkmeans.c
+++ spkmeans.c
@@ -38,36 +38,38 @@ int main(int argc, char *argv[]) {
     curr_vec = head_vec;
     curr_vec->next = NULL;
     while(fscanf(file, "%lf%c", &n, &c) == 2){
-            if (c == '\n')
-            {
-                numpoints++;
-                curr_cord->value = n;
-                curr_vec->cords = head_cord;
-                curr_vec->next = malloc(sizeof( Vector));
-                curr_vec = curr_vec->next;
-                curr_vec->next = NULL;
-                curr_vec->cords = NULL;
-                head_cord = malloc(sizeof( Cord));
-                curr_cord = head_cord;
-                curr_cord->next = NULL;
-                continue;
-            }
-
+        if (c == '\n')
+        {
+            numpoints++;
             curr_cord->value = n;
-            curr_cord->next = malloc(sizeof( Cord));
-            curr_cord = curr_cord->next;
+            curr_vec->cords = head_cord;
+            curr_vec->next = malloc(sizeof( Vector));
+            curr_vec = curr_vec->next;
+            curr_vec->next = NULL;
+            curr_vec->cords = NULL;
+            head_cord = malloc(sizeof( Cord));
+            curr_cord = head_cord;
             curr_cord->next = NULL;
+            continue;
         }
 
-    curr_vec = head_vec;
-    jacboi_mat = calloc(numpoints, sizeof(double *));
-    for(i = 0; i < numpoints; i ++){
-        jacboi_mat[i] = calloc(numpoints, sizeof(double));
-        for(j=0; j < numpoints; j++){
-            jacboi_mat[i][j] = curr_vec->cords->value;
-            curr_vec->cords = curr_vec->cords->next;
+        curr_cord->value = n;
+        curr_cord->next = malloc(sizeof( Cord));
+        curr_cord = curr_cord->next;
+        curr_cord->next = NULL;
+    }
+    if(strcmp(command, "jacobi") == 0){
+        curr_vec = head_vec;
+        jacboi_mat = calloc(numpoints, sizeof(double *));
+        for(i = 0; i < numpoints; i ++)
+        {
+            jacboi_mat[i] = calloc(numpoints, sizeof(double));
+            for(j=0; j < numpoints; j++){
+                jacboi_mat[i][j] = curr_vec->cords->value;
+                curr_vec->cords = curr_vec->cords->next;
+            }
+            curr_vec = curr_vec->next;
         }
-        curr_vec = curr_vec->next;
     }
 
 
